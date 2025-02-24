2cddb8c32a92dc64c3184dfba1e2059005d2aa67
mossaabe <el7amzaouimos3ab@gmail.com>
2025-02-24 22:52:54 +0000

    hero

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -22,7 +22,7 @@ const Hero: React.FC = () => {
 
       <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-90 z-10"></div>
 
-      <div className="absolute md:top-1/2 md:left-1/2 md:transform md:-translate-x-1/2 md:-translate-y-1/2 text-center mt-40 md:mt-0 text-white z-20">
+      <div className="absolute md:top-1/2 md:left-1/2 md:transform md:-translate-x-1/2 md:-translate-y-1/2 text-center mt-48 md:mt-0 text-white z-20">
           <Image 
             src="/logos/01.gif" 
             alt="Hero" 
@@ -32,7 +32,7 @@ const Hero: React.FC = () => {
             className="max-w-full h-auto"
  
           />
-        <button ref={triggerButtonRef} className="bg-white  hover:bg-gray-400 px-6 py-2 text-black hover:bg-gray-400px-6 md:mt-20 md:mt-1 transition-all duration-300" >
+        <button ref={triggerButtonRef} className="bg-white  hover:bg-gray-400 px-6 py-2 text-black hover:bg-gray-400px-6 md:mt-20 mt-1 transition-all duration-300" >
           اضغط وابدأ الرحلة   
         </button>
         
